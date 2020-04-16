1c6ae6bc1c5f0d5f1660c738d818c1e318346adf
Oleg Konovalov <oleg.konovalov@happytravel.com>
2020-04-16 04:13:04 +0300

    Add methods to get all countries and localities

--- LocationsNamesNormalizer/DefaultLocationNameSelector.cs
+++ LocationsNamesNormalizer/DefaultLocationNameSelector.cs
@@ -1,5 +1,4 @@
 using System.Collections.Generic;
-using System.IO;
 using System.Linq;
 using LocationsNamesNormalizer.Models;
 
@@ -35,6 +34,53 @@ namespace LocationsNamesNormalizer
         }
 
 
+        public List<string> GetCountryNames(string countryName)
+        {
+            var country = GetCountry(countryName);
+            var countries = new List<string>();
+            
+            if (country is null)
+                return countries;
+            
+            countries.AddRange(country.Names);
+
+            return countries;
+        }
+
+
+        public List<string> GetLocalityNames(string countryName, string localityName)
+        {
+            var country = GetCountry(countryName);
+            var localities = new List<string>();
+
+            if (country is null)
+                return localities;
+
+            var locality = GetLocality(country, localityName);
+
+            if (locality is null)
+                return localities;
+            
+            localities.AddRange(locality.Names);
+
+            return localities;
+        }
+
+
+        private Country GetCountry(string countryName)
+        {
+            return _countries.SingleOrDefault(c => c.KeyName.ToUpperInvariant().Equals(countryName.ToUpperInvariant())) 
+                ?? _countries.SingleOrDefault(c => c.Names.Select(n => n.ToUpperInvariant()).Contains(countryName.ToUpperInvariant()));
+        }
+
+
+        private Locality GetLocality(Country country, string localityName)
+        {
+            return country.Localities.SingleOrDefault(l => l.KeyName.ToUpperInvariant().Equals(localityName.ToUpperInvariant()))
+                ?? country.Localities.SingleOrDefault(l => l.Names.Select(i => i.ToUpperInvariant()).Contains(localityName.ToUpperInvariant()));
+        }
+        
+        
         private readonly INameNormalizer _nameNormalizer;
         private static List<Country> _countries;
     }

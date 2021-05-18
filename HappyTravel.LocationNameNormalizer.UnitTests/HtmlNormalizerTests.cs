150b6284e59314f4d1082f62f7058243c600b305
Hasmik <hasmik_harutyunyan@outlook.com>
2021-05-18 17:21:27 +0400

    replaced br tags to whitespace where needed

--- HappyTravel.LocationNameNormalizer.UnitTests/HtmlNormalizerTests.cs
+++ HappyTravel.LocationNameNormalizer.UnitTests/HtmlNormalizerTests.cs
@@ -9,11 +9,13 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         [Fact]
         public void Should_remove_attributes()
         {
-            var html = @"<p ar=""jkjl""><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var html =
+                @"<p ar=""jkjl""><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
-            var outerHtml = @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
@@ -26,11 +28,13 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         [Fact]
         public void Should_replace_div_to_br()
         {
-            var html = @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var html =
+                @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from <div>Al Wahda Mall and Caracal Shooting Club.</div> This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
-            var outerHtml = @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from <br>Al Wahda Mall and Caracal Shooting Club.<br> This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
@@ -43,11 +47,13 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         [Fact]
         public void Should_remove_not_needed_tags()
         {
-            var html = @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var html =
+                @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from <input>Al Wahda Mall and Caracal Shooting Club.</input> This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
-            var outerHtml = @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
@@ -60,11 +66,13 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         [Fact]
         public void Should_replace_tags()
         {
-            var html = @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var html =
+                @"<p><b>Property Location</b> <br />With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from <strong>Al Wahda Mall and Caracal Shooting Club.</strong> This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
-            var outerHtml = @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><b>Property Location</b> <br>With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from <b>Al Wahda Mall and Caracal Shooting Club.</b> This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
@@ -77,14 +85,16 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         [Fact]
         public void Should_process_h1_tags()
         {
-            var html = @"<p><h1>Property <i>Location</i></h1> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var html =
+                @"<p><h1>Property <i>Location</i></h1> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>
     <h1><h1>Property <i>Location</i></h1> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</h1>";
 
-            var outerHtml = @"<p><b>Property <i>Location</i></b><br> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><b>Property <i>Location</i></b><br> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>
     <h1>Property Location With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
@@ -105,7 +115,8 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.<br /><br /><br /></p>";
 
-            var outerHtml = @"<p><ul>Property Location</ul> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
+            var outerHtml =
+                @"<p><ul>Property Location</ul> With a stay at Marriott Hotel Downtown, Abu Dhabi, you''ll be centrally located in Abu
     Dhabi, steps from Al Wahda Mall and Caracal Shooting Club. This 5-star hotel is within close proximity of Shaikh
     Khalifa Medical City and Al Nahyan Stadium.</p>";
 
@@ -138,10 +149,20 @@ namespace HappyTravel.LocationNameNormalizer.UnitTests
         public void Should_remove_empty_div_tags()
         {
             var html = @"<html><body><b>Some text</b><div>   </div><br>Text after br.</html></body>";
-            
+
             var result = html.NormalizeInlineHtml();
 
-            Assert.True(result == "<b>Some text</b><br>Text after br.");   
+            Assert.True(result == "<b>Some text</b><br>Text after br.");
+        }
+
+        [Fact]
+        public void Should_replace_br_tags_to_space_when_exists_only_br_tags()
+        {
+            var html = @"<br>Catch some<br><br><br><br>rays on Jumeirah Al Naseem''s private beach or spend the day relaxing at the full-service spa. Then enjoy a<br><br>";
+
+            var result = html.NormalizeInlineHtml();
+
+            Assert.True(result == "Catch some rays on Jumeirah Al Naseem''s private beach or spend the day relaxing at the full-service spa. Then enjoy a");
         }
     }
 }
